a1084b488b8d12ece03b5f421315a4dbe980508f
Hexagons <anton.heestand@gmail.com>
2020-07-10 15:19:57 +0200

    clean up

--- Package.swift
+++ Package.swift
@@ -7,17 +7,14 @@ let package = Package(
     platforms: [
         .iOS(.v11),
         .macOS(.v10_13),
-//        .tvOS(.v11)
     ],
     products: [
-        .library(name: "PixelMerger", targets: ["PixelMerger"]),
+        .library(name: "PixelMerger", targets: ["PixelMerger"])
     ],
     dependencies: [
-//        .package(url: "https://github.com/hexagons/LiveValues.git", .exact("1.2.1")),
-//        .package(url: "https://github.com/hexagons/RenderKit.git", .exact("0.4.6")),
-        .package(url: "https://github.com/hexagons/PixelKit.git", from: "1.1.5"), // .exact("1.0.10")
+        .package(url: "https://github.com/hexagons/PixelKit.git", from: "1.1.5")
     ],
     targets: [
-        .target(name: "PixelMerger", dependencies: ["RenderKit", "PixelKit"])
+        .target(name: "PixelMerger", dependencies: ["PixelKit"])
     ]
 )
